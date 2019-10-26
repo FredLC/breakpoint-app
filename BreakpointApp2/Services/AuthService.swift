0360a7a7df60885fd8092455e36f84ce934a011a
fredlc <fredlefevre@outlook.com>
2019-10-26 11:54:32 -0400

    Integrated Firebase and added Data and Auth services

---
+++ BreakpointApp2/Services/AuthService.swift
@@ -0,0 +1,41 @@
+//
+//  AuthService.swift
+//  BreakpointApp2
+//
+//  Created by Fred Lefevre on 2019-10-26.
+//  Copyright © 2019 Fred Lefevre. All rights reserved.
+//
+
+import Foundation
+import Firebase
+
+class AuthService {
+    
+    static let instance = AuthService()
+    
+    func registerUser(withEmail email: String, andPassword password: String, userCreationComplete: @escaping (_ status: Bool, _ error: Error?) -> ()) {
+        
+        Auth.auth().createUser(withEmail: email, password: password) { (authResult, error) in
+            guard let user = authResult?.user else {
+                userCreationComplete(false, error)
+                return
+            }
+            let userData = ["provider": user.providerID, "email": user.email]
+            DataService.instance.createDBUser(uid: user.uid, userData: userData)
+            userCreationComplete(true, nil)
+        }
+    }
+    
+    
+    func loginUser(withEmail email: String, andPassword password: String, loginComplete: @escaping (_ status: Bool, _ error: Error?) -> ()) {
+        Auth.auth().signIn(withEmail: email, password: password) { (user, error) in
+            if error != nil {
+                loginComplete(false, error)
+                return
+            }
+            loginComplete(true, nil)
+        }
+    }
+    
+    
+}
---
+++ Pods/Target Support Files/Pods-BreakpointApp2/Pods-BreakpointApp2-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_BreakpointApp2 : NSObject
+@end
+@implementation PodsDummy_Pods_BreakpointApp2
+@end
