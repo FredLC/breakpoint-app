3721acf9b772979c5eec90d070ce91c1f4bc957d
fredlc <fredlefevre@outlook.com>
2019-10-26 12:23:41 -0400

    LoginVC UI and InsetTextField view class

---
+++ BreakpointApp2/Controller/AuthVC.swift
@@ -0,0 +1,30 @@
+//
+//  AuthVC.swift
+//  BreakpointApp2
+//
+//  Created by Fred Lefevre on 2019-10-26.
+//  Copyright © 2019 Fred Lefevre. All rights reserved.
+//
+
+import UIKit
+
+class AuthVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
