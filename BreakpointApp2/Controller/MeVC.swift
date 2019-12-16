98728a008b5b07d7143bba24e529451a91c76704
fredlc <fredlefevre@outlook.com>
2019-12-16 16:50:08 -0500

    Integrated MeVC

---
+++ BreakpointApp2/Controller/MeVC.swift
@@ -0,0 +1,25 @@
+//
+//  MeVC.swift
+//  BreakpointApp2
+//
+//  Created by Fred Lefevre on 2019-12-16.
+//  Copyright © 2019 Fred Lefevre. All rights reserved.
+//
+
+import UIKit
+
+class MeVC: UIViewController {
+
+    @IBOutlet weak var profileImage: UIImageView!
+    @IBOutlet weak var emailLabel: UILabel!
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBAction func logoutButtonPressed(_ sender: Any) {
+    }
+    
+    
+}
