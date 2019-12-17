1dbe9617d14c0eb3d3cb946d547b25aed706392c
fredlc <fredlefevre@outlook.com>
2019-12-17 11:01:42 -0500

    Implemented feed table view cell

---
+++ BreakpointApp2/View/FeedCell.swift
@@ -0,0 +1,23 @@
+//
+//  FeedCell.swift
+//  BreakpointApp2
+//
+//  Created by Fred Lefevre on 2019-12-17.
+//  Copyright © 2019 Fred Lefevre. All rights reserved.
+//
+
+import UIKit
+
+class FeedCell: UITableViewCell {
+
+    @IBOutlet weak var profileImage: UIImageView!
+    @IBOutlet weak var emailLabel: UILabel!
+    @IBOutlet weak var contentLabel: UILabel!
+    
+    func configureCell(profileImage: UIImage, email: String, content: String) {
+        self.profileImage.image = profileImage
+        self.emailLabel.text = email
+        self.contentLabel.text = content
+    }
+    
+}
